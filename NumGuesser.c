5241109d756991e4553f57000f724a127e70540a
brozone700 <brozone700@gmail.com>
2026-09-05 03:27:35 +0000

    fixed and hopefully "finished" the code

--- NumGuesser.c
+++ NumGuesser.c
@@ -12,8 +12,11 @@ int main() {
 	int totalTurns = 0;
 	int games = 0;
 	int turns = 0;
+	int lose = 1;
+	int wins = 0;
+	int goodNum = 0;
 
-	printf("What is your name?");
+	printf("\nWhat is your name?   ");
 	scanf("%s", username);
 	while(replay == 1) {
 		guess = 0;
@@ -24,75 +27,106 @@ int main() {
 
 		// The beginning of the game loop
 		while(KeepGoing < 10) {
+			goodNum = 0;
 			turns++;
-			printf("Okay %s this is turn %d \n", username, turns);
-			printf("Enter a number 1-100:");
+			printf("\nOkay %s this is turn %d \n", username, turns);
+			printf("Enter a number 1-100:   ");
 			scanf("%d", &guess);
 
 			// The code for telling the user if they're in the correct number range
 			if (guess > 0 ) {
 				if (guess < 101) {
 					KeepGoing++;
+					goodNum = 1;
 				} else {
+					turns--;
 					printf("That number is too high, it needs to be below 101");
 				}
 			} else {
+				turns--;
 				printf("That number is too low, it needs to be above 0");
 			}
 			
 			// The code for giving the user feedback
-			if (guess > correctNum) {
-				temp = guess-50;
-				if (temp > correctNum) {
-					printf("You're way too high");
-				} else {
-					printf("You need to go lower");
-				}
-			} else if (guess < correctNum) {
-				temp = guess + 50;
-					if (temp < correctNum) {
-						printf("You're way too low");
-					} else {
-						printf("You need to go higher");
+			if (goodNum == 1) {
+				if (guess > correctNum) {
+					if (KeepGoing < 10) {
+						temp = guess-50;
+						if (temp > correctNum) {
+							printf("You're way too high");
+						} else {
+							printf("You need to go lower");
+						}
 					}
-			}else if (guess == correctNum) {
-				printf("Wow, you actually got it right");
-				totalTurns += turns;
-				games++;
-				KeepGoing = 10;
-				win = 1;
+				} else if (guess < correctNum) {
+					if (KeepGoing < 10) {
+						temp = guess + 50;
+						if (temp < correctNum) {
+							printf("You're way too low");
+						} else {
+							printf("You need to go higher");
+						}
+					}
+				}else if (guess == correctNum) {
+					printf("Wow, you actually got it right.");
+					wins++;
+					KeepGoing = 10;
+					lose = 0;
+				}
 			}
+			printf("\n");
 		}
+		totalTurns += turns;
+		games++;
 			// results
 		if (turns == 1) {
 			printf("You won in %d turn.\nThats amazing!", turns);
-		} else if (turns >= 7){
-			printf("You won in %d turns.\nTry to do a little better next time.", turns);
-		} else if {
+		} else if (turns >= 7) {
+			if (lose == 0) { 
+				printf("You won in %d turns.\nTry to do a little better next time.", turns);
+			} else {
+				printf("Wow, you actually lost, do better next time. The correct number was %d.", correctNum);
+			}
+		} else {
 			printf("You won in %d turns.\nNot bad.", turns);
+		}
+		printf("\n");
+		if (lose == 0) {
+			wins++;
 		}
 
 		// current stats
 		if (games == 1) {
 			if (totalTurns == 1) {
 				printf("So far you've played 1 game and only needed 1 turn to beat it.");
-			} else {
+			} else if ( lose == 0) {
 				printf("So far you've played 1 game and it took you %d turns to beat it.", turns);
+			} else {
+				printf("So far you've lost the only game that you've played");
+			}
+		} else if (wins == games) {
+			if (games > 2 ) {
+				printf("\nSo far you've played %d games, winning all of them, and its taken you %d turns to beat them.", games, totalTurns);
+			} else {
+				printf("\nSo far you've played %d games, winning both of them, and its taken you %d turns to beat them.", games, totalTurns);
 			}
 		} else {
-			printf("\nSo far you've played %d games and its taken you %d turns to beat them.", games, turns);
+			printf("\nSo far you've played %d games, winning %d of them, and its taken you %d turns to beat them.", games, wins, totalTurns);
 		}
+		turns = 0;
 		printf("\nWould you like to play again 1/0 (1 being yes and 0 being no):");
 		int again = 0;
-		scanf(" %d", again);
+		scanf(" %d", &again);
 		if (again == 0) {
 			printf("Okay, goodbye");
-			replay = 0
+			replay = 0;
 		} else if (again == 1) {
 			KeepGoing = 0;
 			for (int i = 0; i < 10; i++) {
 				printf("\n");
 			}
+		} else {
+			printf("that wasn't one of the choices, please choose either 1 or 0");
 		}
 	}
 }
